52736d7f68240cbd63a309354caebf6b3e9f6693
Geronimo98456 <111605719+Geronimo98456@users.noreply.github.com>
2022-11-18 14:44:30 -0300

    Add files via upload

---
+++ proyecto 2/proyecto2/index.js
@@ -0,0 +1,42 @@
+/* Numericos */
+let edad = 30;
+
+/* String */
+
+let nombre = "Luis";
+
+/* Booleano */
+
+let asistencia = true; /* false */
+
+/* NaN */
+
+let operacion = 4/"J";
+let lista = document.querySelector("#Peliculas mas valoradas")
+
+/* NULL */
+
+let esperando = null;
+
+/* console.log(esperando);
+ *//* 2 hr despues  */
+
+esperando = "Llegó el valor";
+
+
+/* undefined */
+
+let indefinida;
+
+/* console.log(indefinida); */
+
+indefinida = "Ya no es undefinded";
+
+/* console.log(indefinida); */
+
+
+/* operadores de comparación devuelven BOOLEANOS */
+
+let menor = 15 >= 100;
+
+console.log(menor);
